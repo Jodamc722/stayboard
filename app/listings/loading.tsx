27ce61ab44964c90733baf31bfe74dbe60435f2b
Jodamc722 <Jodamc722@gmail.com>
2026-08-11 11:12:51 -0400

    Delete app/listings/loading.tsx

--- app/listings/loading.tsx
+++
@@ -1,3 +0,0 @@
-import { Shell } from '@/components/Shell'
-import { PageSkeleton } from '@/components/Skeleton'
-export default function Loading() { return <Shell><PageSkeleton title="Properties" /></Shell> }
